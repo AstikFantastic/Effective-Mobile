a0620a8074f35f539c8bd08b2e5801c84ddddfb3
AstikFantastic <astimir.maryshev@gmail.com>
2026-01-21 23:41:31 +0300

    Added: header, searchBar, buttomSafeArea, newTasksButton, all tasks label

--- Effective Mobile/View/MainViewController/MainViewController.swift
+++ Effective Mobile/View/MainViewController/MainViewController.swift
@@ -12,8 +12,18 @@ final class MainViewController: UIViewController, MainViewControllerProtocol {
     
     private var todos: [Todo] = []
     private let tableView = UITableView()
-    
+    private let headerLabel = UILabel()
     private let searchBar = UISearchBar()
+    private let bottomSafeArea = UIView()
+    private let allTasksCount = UILabel()
+    private let addNewTask = UIButton()
+    let image = UIImage(
+        systemName: "square.and.pencil",
+        withConfiguration: UIImage.SymbolConfiguration(
+            pointSize: 24,
+            weight: .regular
+        )
+    )
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,8 +35,21 @@ final class MainViewController: UIViewController, MainViewControllerProtocol {
     
     // MARK: - Setup UI
     func setupUI() {
-
+        
         view.backgroundColor = .black
+        
+        headerLabel.text = "Tasks"
+        headerLabel.textColor = .white
+        headerLabel.font = UIFont.systemFont(ofSize: 34, weight: .bold)
+        headerLabel.textAlignment = .left
+        
+        view.addSubview(headerLabel)
+        
+        searchBar.placeholder = "Search"
+        searchBar.backgroundColor = .darkGray
+        searchBar.barTintColor = .black
+        
+        view.addSubview(searchBar)
         
         tableView.register(ToDoListCell.self, forCellReuseIdentifier: ToDoListCell.identifier)
         tableView.backgroundColor = .black
@@ -37,12 +60,51 @@ final class MainViewController: UIViewController, MainViewControllerProtocol {
         
         view.addSubview(tableView)
         
-        tableView.translatesAutoresizingMaskIntoConstraints = false
+        bottomSafeArea.backgroundColor = .darkGray
+        
+        view.addSubview(bottomSafeArea)
+        
+        allTasksCount.text = "\(todos.count) tasks"
+        allTasksCount.textColor = .white
+        allTasksCount.font = UIFont.systemFont(ofSize: 11)
+        
+        view.addSubview(allTasksCount)
+        
+        addNewTask.setImage(image, for: .normal)
+        addNewTask.imageView?.tintColor = .yellow
+
+        
+        view.addSubview(addNewTask)
+        
+        [headerLabel, tableView, searchBar, bottomSafeArea, allTasksCount, addNewTask].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
         NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            
+            headerLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            headerLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            
+            searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            searchBar.topAnchor.constraint(equalTo: headerLabel.bottomAnchor, constant: 5),
+            
+            tableView.topAnchor.constraint(equalTo: searchBar.bottomAnchor, constant: 5),
+            tableView.bottomAnchor.constraint(equalTo: bottomSafeArea.topAnchor),
             tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            bottomSafeArea.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            bottomSafeArea.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            bottomSafeArea.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            bottomSafeArea.heightAnchor.constraint(equalToConstant: 83),
+            
+            allTasksCount.centerXAnchor.constraint(equalTo: bottomSafeArea.centerXAnchor),
+            allTasksCount.centerYAnchor.constraint(equalTo: bottomSafeArea.centerYAnchor, constant: -20),
+            
+            addNewTask.centerYAnchor.constraint(equalTo: allTasksCount.centerYAnchor),
+            addNewTask.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+
         ])
     }
     
